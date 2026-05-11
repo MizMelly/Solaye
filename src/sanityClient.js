3de3209b94c2814005e4f61ca98d9b0a422fcfb8
MizMelly <jazminelike@yahoo.com>
2026-05-11 22:10:30 +0100

    updates

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,14 +1,14 @@
 import { createClient } from "@sanity/client";
-import { createImageUrlBuilder } from "@sanity/image-url";
-
+import imageUrlBuilder from "@sanity/image-url"; // keep default import
 
 export const client = createClient({
-  projectId: "5aio0jd9",
+  projectId: "5aio0jd9", 
   dataset: "production",
   useCdn: true,
   apiVersion: "2025-01-01",
 });
-// IMAGE BUILDER (VERY IMPORTANT)
+
+// image builder
 const builder = imageUrlBuilder(client);
 
 export const urlFor = (source) => builder.image(source);
